5e026e59a242034583c872847f7eaca2a47ed531
EvgeniiGurenkov <evg-gurenkov@yandex.ru>
2022-05-10 13:05:00 +0400

    решена вторая задача

---
+++ 2nd/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = new int [3];
+Console.WriteLine("Введите 3 числа: ");
+int MaxNumber = numbers[0];
+for (byte i = 0; i < numbers.Length; i++)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbers[i] = number;
+}
+for (byte i = 0; i < numbers.Length; i++)
+{
+    if (MaxNumber < numbers[i])
+    {
+        MaxNumber = numbers[i];
+    }
+}
+Console.WriteLine("Max = "+MaxNumber); 
