a96d9267a7c04202cb2d32c9ad66a1c3443b5bb3
EvgeniiGurenkov <evg-gurenkov@yandex.ru>
2022-05-10 12:57:39 +0400

    решена первая задача

---
+++ 1st/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+if (Number1 > Number2)
+{
+    Console.WriteLine("Max = " + Number1+", Min = "+ Number2);
+}
+if (Number1 < Number2)
+{
+    Console.WriteLine("Max = " + Number2+", Min = "+ Number1);
+
+}
+else
+{
+    Console.WriteLine("Max и Min значения не найдены! Введены одинаковые числа.");
+}
+
