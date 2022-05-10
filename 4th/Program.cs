ae9d948f220d7f4fed8c774529c7c7d34f67bf35
EvgeniiGurenkov <evg-gurenkov@yandex.ru>
2022-05-10 13:13:29 +0400

    Решена четвертая задача.

---
+++ 4th/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Найдены следующие чётные числа: ");
+if (number == 0)
+{
+    Console.Write(number);
+}
+    for (int i = 1; i <= number; i++)
+    {
+
+        if (i % 2 == 0)
+        {
+            Console.Write(i);
+            if (i != number & i!= number-1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+    }
